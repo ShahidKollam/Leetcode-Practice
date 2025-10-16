981fe654fe28a4e0e47764c6d890a7a081e0e7ec
Shahid M <shahidazhari313@gmail.com>
2025-10-16 23:16:33 +0530

    Time: 5 ms (78.94%) | Memory: 63.8 MB (67.86%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    let str = String(x)
+    let left = 0
+    let right = str.length - 1
+    while (left < right) {
+        if (str[left] === str[right]) {
+            left++
+            right--
+        } else {
+            return false
+        }
+    }
+    return true
+};
