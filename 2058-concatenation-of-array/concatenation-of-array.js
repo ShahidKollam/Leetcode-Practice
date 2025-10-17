5998c90f7b4b5ff6cf8608fd91c2ad4fa75dd88c
Shahid M <shahidazhari313@gmail.com>
2025-10-17 12:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (68.66%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,5 +3,12 @@
  * @return {number[]}
  */
 var getConcatenation = function (nums) {
-    return [...nums, ...nums]
+
+    const n = nums.length
+    // nums.length = n * 2
+
+    for (let i = 0; i < n; i++) {
+        nums[n + i] = nums[i]
+    }
+    return nums
 };
