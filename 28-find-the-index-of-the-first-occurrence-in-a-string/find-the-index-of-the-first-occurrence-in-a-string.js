f93a86e6489063ad60bb223b034374b781ef5979
Shahid M <shahidazhari313@gmail.com>
2025-10-20 14:32:08 +0530

    Time: 0 ms (100.00%) | Memory: 54.7 MB (22.78%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,6 +4,5 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-    if (haystack.includes(needle)) return haystack.indexOf(needle)
-    else return -1
+   return haystack.indexOf(needle);
 };
