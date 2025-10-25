91e87523abc3de28a3771b8b1a18f7e215567d1a
Shahid M <shahidazhari313@gmail.com>
2025-10-25 19:48:12 +0530

    Time: 5 ms (23.77%) | Memory: 61.7 MB (6.10%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,16 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-
-    let j = 0
+    let l = 0
+    let r = 0
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== 0) {
-            nums[j] = nums[i]
-            j++
+            [nums[l], nums[r]] = [nums[r], nums[l]]
+            l++
         }
-    }
-    while (j < nums.length) {
-        nums[j] = 0
-        j++
+        r++
     }
 };
