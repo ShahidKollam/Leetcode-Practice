b0685d173f941d41dae475d59aa0f5fe0475e860
Shahid M <shahidazhari313@gmail.com>
2025-10-25 18:58:55 +0530

    Time: 15 ms (84.38%) | Memory: 70.6 MB (87.59%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,11 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    let set = new Set()
-    for (let i = 0; i < nums.length; i++) {
-        if (set.has(nums[i])) return true
-        set.add(nums[i])
-    }
-    return false
+   return new Set(nums).size < nums.length;
 };
 
