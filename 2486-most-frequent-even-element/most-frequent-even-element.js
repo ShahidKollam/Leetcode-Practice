6e880e2c1d74caee130441368154084a42a75be2
Shahid M <shahidazhari313@gmail.com>
2025-10-27 12:32:45 +0530

    Time: 10 ms (94.24%) | Memory: 62.2 MB (82.73%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.js
+++ 2486-most-frequent-even-element/most-frequent-even-element.js
@@ -2,40 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var mostFrequentEven = function (nums) {
-    let freq = 0
-    let small = 0
+var mostFrequentEven = function(nums) {
+    let map = new Map();
+    let freq = 0;
+    let small = Infinity;
 
-    let map = new Map()
     for (let n of nums) {
         if (n % 2 === 0) {
-            if (map.has(n)) {
-                map.set(n, map.get(n) + 1)
-            } else {
-                map.set(n, 1)
+            let count = (map.get(n) || 0) + 1;
+            map.set(n, count);
+
+            if (count > freq || (count === freq && n < small)) {
+                freq = count;
+                small = n;
             }
-        }
-        if (freq < map.get(n)) {
-            freq = map.get(n)
-            small = n
-        } else if (freq === map.get(n) && small > n) {
-            small = n
         }
     }
 
-    if (map.size === 0) return -1
-    return small
-
-    // let freq = 0
-    // let small = 0
-
-    // for (let [key, val] of map) {
-    //     if (val > freq) {
-    //         freq = val
-    //         small = key
-    //     } else if (val === freq) {
-    //         small = Math.min(small, key)
-    //     }
-    // }
-    // return small
-}
+    return freq === 0 ? -1 : small;
+};
