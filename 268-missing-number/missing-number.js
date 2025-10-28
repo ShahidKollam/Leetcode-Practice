6f3652d5a9bdd6711d5a3a87d5199743d638ee1d
Shahid M <shahidazhari313@gmail.com>
2025-10-28 11:03:04 +0530

    Time: 0 ms (100.00%) | Memory: 56.2 MB (73.98%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,21 +3,8 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-    let missing = 0;
-    let sum = 0;
-    // while (missing <= nums.length) {
-    //     if (!nums.includes(missing)) return missing
-    //     missing++
-    // }
-    for (let i = 0; i <= nums.length; i++) {
-        console.log("i", i);
-        if (nums[i]) {
-            sum += nums[i];
-        }
-        missing += i;
-    }
-    console.log("sum", sum);
-    console.log("missing", missing);
-
-    return missing - sum;
+    let n = nums.length
+    let expectedSum = n * (n + 1) / 2
+    let arraySum = nums.reduce((acc, val) => acc += val, 0)
+    return expectedSum - arraySum
 };
