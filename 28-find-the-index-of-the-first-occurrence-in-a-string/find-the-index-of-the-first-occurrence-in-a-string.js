eb8f087f9a70e9d0771220c5eae7ba2d779bf740
Shahid M <shahidazhari313@gmail.com>
2025-11-08 17:57:44 +0530

    Time: 0 ms (100.00%) | Memory: 54.7 MB (22.13%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,16 +4,5 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-    return haystack.indexOf(needle);
-    for (let i = 0; i < haystack.length; i + 2) {
-        let match = true
-        for (let j = 0; j < needle.length; j++) {
-            if (haystack[i] === needle[j]) {
-                let ans = i - needle.length
-            } else {
-                match = false
-            }
-        }
-    }
-    return match ? ans : -1
+    return haystack.indexOf(needle)
 };
