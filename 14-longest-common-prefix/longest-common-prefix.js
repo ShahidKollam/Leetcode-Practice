3886f0bca0e27d5348b73b1d67ee78a32f2f1f34
Shahid M <shahidazhari313@gmail.com>
2025-11-08 11:16:35 +0530

    Time: 1 ms (54.91%) | Memory: 56.2 MB (23.66%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,17 +3,28 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-    strs = strs.sort()
-    let str = ''
-    let first = strs[0]
-    let last = strs[strs.length - 1]
+    // strs = strs.sort()
+    // let str = ''
+    // let first = strs[0]
+    // let last = strs[strs.length - 1]
 
-    for (let i = 0; i < last.length; i++) {
-        if (first[i] === last[i]) {
-            str += first[i]
-        } else {
-            break
+    // for (let i = 0; i < last.length; i++) {
+    //     if (first[i] === last[i]) {
+    //         str += first[i]
+    //     } else {
+    //         break
+    //     }
+    // }
+    // return str
+    if (!strs.length) return ""
+
+    for (let i = 0; i < strs[0].length; i++) {
+        let char = strs[0][i]
+        for (let j = 0; j < strs.length; j++) {
+            if (char !== strs[j][i]) {
+                return strs[0].slice(0, i)
+            }
         }
     }
-    return str
+    return strs[0]
 };
