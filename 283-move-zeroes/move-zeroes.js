3061e5945b81f0e2af62646d11a0c46f5e0fadfc
Shahid M <shahidazhari313@gmail.com>
2025-11-08 17:26:15 +0530

    Time: 0 ms (100.00%) | Memory: 61.7 MB (5.58%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -1,16 +1,10 @@
-/**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
- */
 var moveZeroes = function (nums) {
-    let left = 0;
+    let insertPosition = 0; // position to place the next non-zero number
 
-    for (let right = 0; right < nums.length; right++) {
-        
-        if (nums[right] !== 0) {
-
-            [nums[left], nums[right]] = [nums[right], nums[left]];
-            left++;
+    for (let current = 0; current < nums.length; current++) {
+        if (nums[current] !== 0) {
+            [nums[current], nums[insertPosition]] = [nums[insertPosition], nums[current]];
+            insertPosition++;
         }
     }
 };
