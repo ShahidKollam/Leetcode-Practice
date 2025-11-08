540d344bc83680ab20b335adc5e563db1300ca03
Shahid M <shahidazhari313@gmail.com>
2025-11-08 17:44:33 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (97.68%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,7 +3,20 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-    nums = nums.sort()
-    let mid = Math.floor(nums.length / 2)
-    return nums[mid]
+    // nums = nums.sort()
+    // let mid = Math.floor(nums.length / 2)
+    // return nums[mid]
+    let count = 0
+    let candidate = nums[0]
+    for (let i = 0; i < nums.length; i++) {
+        if (count === 0) {
+            count++
+            candidate = nums[i]
+        } else if (candidate === nums[i]) {
+            count++
+        } else {
+            count--
+        }
+    }
+    return candidate
 };
