c9a1f16e9341ce76d203842fc3f5d3464b3dc9b0
Shahid M <shahidazhari313@gmail.com>
2025-11-09 13:09:15 +0530

    Time: 12 ms (71.85%) | Memory: 58.5 MB (51.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,21 +3,19 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function (s, t) {
-    if (s.length !== t.length) return false
-    let map = new Map()
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
 
-    for (let i = 0; i < t.length; i++) {
-        map.set(t[i], (map.get(t[i]) || 0) + 1)
-    }
+    let count = {};
+
     for (let ch of s) {
-        if (!map.has(ch)) {
-            return false
-        }
-        map.set(ch, map.get(ch) - 1)
-
-        if (map.get(ch) < 0) return false
-
+        count[ch] = (count[ch] || 0) + 1;
     }
-    return true
+
+    for (let ch of t) {
+        if (!count[ch]) return false;
+        count[ch]--;
+    }
+
+    return true;
 };
