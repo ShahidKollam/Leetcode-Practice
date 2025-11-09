e1c3359f262f3a059a3fc86c786d5cce31c3f8ce
Shahid M <shahidazhari313@gmail.com>
2025-11-09 18:12:25 +0530

    Time: 9 ms (18.97%) | Memory: 61.4 MB (10.77%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.js
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var differenceOfSum = function (nums) {
-    let sum1 = 0
-    let sum2 = 0
+    let digisum = nums.reduce((acc, val) => acc += val, 0)
 
+    let arrsum = 0
     for (let i = 0; i < nums.length; i++) {
-        sum1 += nums[i]
-
-        let num = nums[i]
-        while (num > 0) {
-            sum2 += num % 10
-            num = Math.floor(num / 10)
+        let sum = 0
+        if (nums[i] > 9) {
+            sum = String(nums[i]).split('').reduce((acc, val) => acc += Number(val), 0)
+            arrsum += Number(sum)
+        } else {
+            arrsum += nums[i]
         }
     }
-    return sum1 - sum2
+    return digisum - arrsum
 };
