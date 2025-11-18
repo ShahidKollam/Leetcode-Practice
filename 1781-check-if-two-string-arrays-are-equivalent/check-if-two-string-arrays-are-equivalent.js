621ba2dea331c3699362f8a587fd0defb21199a8
Shahid M <shahidazhari313@gmail.com>
2025-11-18 09:38:19 +0530

    Time: 1 ms (10.48%) | Memory: 54.5 MB (40.00%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
@@ -4,7 +4,22 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function (word1, word2) {
-    word1 = word1.join("")
-    word2 = word2.join("")
-    return word1 === word2
+    let i = 0, j = 0
+    let x = 0, y = 0
+
+    while (i < word1.length && j < word2.length) {
+        if (word1[i][x] !== word2[j][y]) return false
+
+        x++
+        if (x === word1[i].length) {
+            x = 0
+            i++
+        }
+        y++
+        if (y === word2[j].length) {
+            y = 0
+            j++
+        }
+    }
+    return i === word1.length && j === word2.length
 };
