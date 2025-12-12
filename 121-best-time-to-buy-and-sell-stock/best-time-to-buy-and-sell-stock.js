e1e51e3cbbe71315adae088b93bdd68341f050fc
Shahid M <shahidazhari313@gmail.com>
2025-12-12 11:28:56 +0530

    Time: 2 ms (81.18%) | Memory: 66.2 MB (19.05%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,11 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-    let min = prices[0]
-
-    let max = 0
+    let maxprofit = 0
+    let minbuy = prices[0]
     for (let i = 0; i < prices.length; i++) {
-        min = Math.min(prices[i], min)
-        max = Math.max(max, prices[i] - min)
-
+        minbuy = Math.min(prices[i], minbuy)
+        maxprofit = Math.max(maxprofit, prices[i] - minbuy)
     }
-    return max
+    return maxprofit
 };
