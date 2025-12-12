5991b2156cba19d1ec32985784e6c015467a2105
Shahid M <shahidazhari313@gmail.com>
2025-12-12 09:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (75.94%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -4,18 +4,12 @@
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
-    let arr = []
-    let obj = {}
-    for (let i = 0; i < nums1.length; i++) {
-        obj[nums1[i]] = (obj[nums1[i]] || 0) + 1
-    }
-
-    for (let i = 0; i < nums2.length; i++) {
-        if (obj[nums2[i]] >= 1 && !arr.includes(nums2[i])) {
-            arr.push(nums2[i])
-            obj[nums2[i]] = obj[nums2[i]] - 1
+    let set1 = new Set(nums1)
+    let set2 = new Set()
+    for (let n of nums2) {
+        if (set1.has(n)) {
+            set2.add(n)
         }
     }
-
-    return arr
+    return [...set2]
 };
