37893a8318e6758380cce6e78ab78d56c30b7b82
Shahid M <shahidazhari313@gmail.com>
2025-12-12 12:01:32 +0530

    Time: 2 ms (92.88%) | Memory: 61.4 MB (21.07%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -5,12 +5,11 @@
 var isPalindrome = function (s) {
     let l = 0
     let r = s.length - 1
-
     while (l < r) {
         while (l < r && !/[a-z0-9]/i.test(s[l])) l++
-        while (l < r && !/[a-z0-9]/i.test(s[r])) r-- 
-        if (s[l].toLowerCase() !== s[r].toLowerCase()) { return false }
-
+        while (l < r && !/[a-z0-9]/i.test(s[r])) r--
+        
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
         l++
         r--
     }
