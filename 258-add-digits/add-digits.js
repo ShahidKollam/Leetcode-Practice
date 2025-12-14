ad147feede9e1632a3f4fa9418c06d40f027c70d
Shahid M <shahidazhari313@gmail.com>
2025-12-14 11:24:36 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (81.98%) - LeetSync

--- 258-add-digits/add-digits.js
+++ 258-add-digits/add-digits.js
@@ -3,10 +3,6 @@
  * @return {number}
  */
 var addDigits = function (num) {
-    while (num > 9) {
-        let sum = num.toString().split("")
-        sum = sum.reduce((acc, val) => acc += Number(val), 0)
-        num = sum
-    }
-    return num
+   return num === 0 ? 0 : 1 + (num - 1) % 9;
+
 };
